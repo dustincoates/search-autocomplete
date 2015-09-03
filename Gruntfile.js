9d3769d84d2537ac412a570c9a5ade38bedc5bee
Dustin Coates <dustin@dcoates.com>
2015-09-03 09:20:49 -0400

    Add SCSS underpinings

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,34 @@
 module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
       all: ['Gruntfile.js', 'tasks/**/*.js', 'lib/**/*.js']
+    },
+
+    watch: {
+      src: {
+        files: ['lib/*.js', 'styles/**/*.scss'],
+        tasks: ['assemble']
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'styles/main.css': 'styles/main.scss'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('assemble', ['sass']);
+  grunt.registerTask('default', ['jshint', 'sass']);
 };
